1d1555af587818981811fa97a7de90f6ed5c2648
victorbamikole <victorbamikole92@gmail.com>
2021-12-22 17:02:19 +0100

    updated app

--- app/src/main/java/com/example/closedcircuitapplication/ui/ProjectScreenFragment.kt
+++ app/src/main/java/com/example/closedcircuitapplication/ui/ProjectScreenFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.closedcircuitapplication.R
 import com.example.closedcircuitapplication.databinding.FragmentProjectScreenBinding
 
 
@@ -13,11 +12,6 @@ class ProjectScreenFragment : Fragment() {
     private var _binding : FragmentProjectScreenBinding? = null
     private val binding get() = _binding!!
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
