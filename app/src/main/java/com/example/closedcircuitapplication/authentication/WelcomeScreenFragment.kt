96aea86395b92b10aa13a65e27b5236dfbdc3300
Iniyealakeret2 <iniyealakeret1@gmail.com>
2021-12-24 06:37:22 +0100

    create plan summary implementation

--- app/src/main/java/com/example/closedcircuitapplication/authentication/WelcomeScreenFragment.kt
+++ app/src/main/java/com/example/closedcircuitapplication/authentication/WelcomeScreenFragment.kt
@@ -31,6 +31,10 @@ class WelcomeScreenFragment : Fragment() {
             findNavController().navigate(R.id.action_welcomeScreenFragment_to_loginFragment)
         }
 
+//        binding.click.setOnClickListener {
+//            findNavController().navigate(R.id.action_welcomeScreenFragment_to_createPlanSummaryFragment)
+//        }
+
         // navigate from  welcome screen to create an account
         binding.welcomePageCreateAccountBtn.setOnClickListener {
             findNavController().navigate(R.id.action_welcomeScreenFragment_to_createAccountFragment)
---
+++ app/src/main/java/com/example/closedcircuitapplication/projectSummary/CreatePlanSummaryFragment.kt
@@ -0,0 +1,44 @@
+package com.example.closedcircuitapplication.projectSummary
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.core.content.res.ResourcesCompat
+import com.example.closedcircuitapplication.R
+import com.example.closedcircuitapplication.databinding.FragmentCreatePlanSummaryBinding
+
+
+class CreatePlanSummaryFragment : Fragment(R.layout.fragment_create_plan_summary) {
+
+    private lateinit var binding: FragmentCreatePlanSummaryBinding
+
+    override fun onResume() {
+        super.onResume()
+
+        //select your preferred means of support
+        val options = resources.getStringArray(R.array.support_means)
+        val arrayAdapter = ArrayAdapter(requireContext(),R.layout.dropdown_item,options)
+        binding.dropdownMenuSupport.setAdapter(arrayAdapter)
+        binding.dropdownMenuSupport.setDropDownBackgroundDrawable(ResourcesCompat.getDrawable(resources,R.drawable.text_input_background_dropdown,null))
+
+        //select maximum number of lenders
+        val lenderOption = resources.getStringArray(R.array.number_of_lenders)
+        val arrayAdapterLender = ArrayAdapter(requireContext(),R.layout.dropdown_item,lenderOption)
+        binding.dropdownMenuLenders.setAdapter(arrayAdapterLender)
+        binding.dropdownMenuLenders.setDropDownBackgroundDrawable(ResourcesCompat.getDrawable(resources,R.drawable.text_input_background_dropdown,null))
+
+    }
+
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentCreatePlanSummaryBinding.bind(view)
+
+    }
+
+}
