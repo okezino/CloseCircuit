0fe1736a50d962b301cce49bb7c49df961e3493e
obetta1 <obettabenjamin44@gmail.com>
2022-01-12 18:37:16 +0100

    pull update

---
+++ app/src/main/java/com/example/closedcircuitapplication/ui/sponsorsScreenUi/SponsorsScreenFragment.kt
@@ -0,0 +1,31 @@
+package com.example.closedcircuitapplication.ui.sponsorsScreenUi
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.closedcircuitapplication.R
+import com.example.closedcircuitapplication.databinding.FragmentSponsorsScreenBinding
+
+
+class SponsorsScreenFragment : Fragment() {
+
+    private var _binding : FragmentSponsorsScreenBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding =  FragmentSponsorsScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+}
