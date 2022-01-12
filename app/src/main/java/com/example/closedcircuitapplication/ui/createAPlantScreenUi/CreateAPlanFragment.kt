769e1c1e5fb955ddf010a6593dd8abad9d4932bf
obetta1 <obettabenjamin44@gmail.com>
2022-01-12 19:08:42 +0100

    pull update

--- app/src/main/java/com/example/closedcircuitapplication/ui/createAPlantScreenUi/CreateAPlanFragment.kt
+++ app/src/main/java/com/example/closedcircuitapplication/ui/createAPlantScreenUi/CreateAPlanFragment.kt
@@ -73,9 +73,8 @@ class CreateAPlanFragment : Fragment(), SendImage_UriToCreateAPlanFragment  {
             if (_sector != null && _category != null){
                 Log.d("CREATE_PLAN", "SECTOR=====> $_sector  CATEGORY====> $_category URI====> ")
 
-                findNavController().navigate(R.id.uploadingProofForCompletedStepFragment)
-            //                val action = CreateAPlanFragmentDirections.actionCreateAPlanFragment2ToCreatePlanSummaryFragment2(_category,_sector,_uri,"NGN")
-//                findNavController().navigate(action)
+                            val action = CreateAPlanFragmentDirections.actionCreateAPlanFragment2ToCreatePlanSummaryFragment2(_category,_sector,_uri,"NGN")
+                findNavController().navigate(action)
             }
 
             Log.d("TEST", "SECTOR=====> $sector and CATEGORY====> $category ")
