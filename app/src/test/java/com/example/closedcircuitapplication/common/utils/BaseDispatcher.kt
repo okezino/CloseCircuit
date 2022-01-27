21082438d2c2858e749c87f70ff4efec00ec2110
Oladapo <oladokun104@gmail.com>
2022-01-27 14:46:44 +0100

    CI build fail fix

--- app/src/test/java/com/example/closedcircuitapplication/common/utils/BaseDispatcher.kt
+++ app/src/test/java/com/example/closedcircuitapplication/common/utils/BaseDispatcher.kt
@@ -1,7 +1,6 @@
 package com.example.closedcircuitapplication.common.utils
 
 import com.example.closedcircuitapplication.common.data.network.ClosedCircuitApiEndpoints
-import com.example.closedcircuitapplication.common.utils.resources.SuccessJson
 import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.RecordedRequest
@@ -10,7 +9,6 @@ class BaseDispatcher : Dispatcher() {
     override fun dispatch(request: RecordedRequest): MockResponse {
         return when (request.path) {
             "/${ClosedCircuitApiEndpoints.LOGIN}" -> MockResponse().setResponseCode(200)
-                .setBody(SuccessJson.loginSuccessJson)
             else -> MockResponse().setResponseCode(400)
         }
     }
