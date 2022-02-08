b4ff450c452c9ea00cecdb81619be14d676c19e1
obetta1 <obettabenjamin44@gmail.com>
2022-02-08 20:39:48 +0100

    updated validation

--- app/src/main/java/com/example/closedcircuitapplication/authentication/data/datadto/ForgotPasswordDto.kt
+++
@@ -1,5 +0,0 @@
-package com.example.closedcircuitapplication.authentication.data.datadto
-
-data class ForgotPasswordDto(
-    val message: String
-)
--- app/src/main/java/com/example/closedcircuitapplication/authentication/domain/models/ForgotPasswordRequest.kt
+++
@@ -1,5 +0,0 @@
-package com.example.closedcircuitapplication.authentication.domain.models
-
-data class ForgotPasswordRequest(
-    val email: String
-)
