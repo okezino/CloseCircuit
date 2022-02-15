6dcef134831c51936f9242238bb5de9683c29d21
victorbamikole <victorbamikole92@gmail.com>
2022-02-15 12:07:14 +0100

    create plan step2 screen

--- app/src/main/java/com/example/closedcircuitapplication/plan/presentation/ui/screens/PlanCreatedSuccessfullyFragment.kt
+++ app/src/main/java/com/example/closedcircuitapplication/plan/presentation/ui/screens/PlanCreatedSuccessfullyFragment.kt
@@ -6,19 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.closedcircuitapplication.R
-import com.example.closedcircuitapplication.databinding.FragmentCreateAPlanBinding
 import com.example.closedcircuitapplication.databinding.FragmentCreatePlanStep2Binding
+import com.example.closedcircuitapplication.databinding.FragmentPlanCreatedSucessfullyBinding
 
-class CreatePlanStep2Fragment : Fragment() {
-    private var _binding: FragmentCreatePlanStep2Binding? = null
+class PlanCreatedSuccessfullyFragment : Fragment() {
+
+    private var _binding: FragmentPlanCreatedSucessfullyBinding? = null
     private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        _binding = FragmentCreatePlanStep2Binding.inflate(inflater, container, false)
+        _binding = FragmentPlanCreatedSucessfullyBinding.inflate(inflater, container, false)
         return binding.root
     }
-
 }
