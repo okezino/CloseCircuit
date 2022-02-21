96eb1e0d0449da901f01942e2d3349fa693b6722
victorbamikole <victorbamikole92@gmail.com>
2022-02-21 12:35:36 +0100

    creat plan step2

--- app/src/main/java/com/example/closedcircuitapplication/plan/presentation/ui/screens/ProjectSummaryFragment.kt
+++ app/src/main/java/com/example/closedcircuitapplication/plan/presentation/ui/screens/ProjectSummaryFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.widget.PopupMenu
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
