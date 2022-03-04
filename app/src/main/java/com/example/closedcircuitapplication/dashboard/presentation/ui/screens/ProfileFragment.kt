00752ccdb8291180616e8e3f1af81a1188b4f5ef
victorbamikole <victorbamikole92@gmail.com>
2022-03-04 19:51:35 +0100

    profile screen

--- app/src/main/java/com/example/closedcircuitapplication/dashboard/presentation/ui/screens/ProfileFragment.kt
+++ app/src/main/java/com/example/closedcircuitapplication/dashboard/presentation/ui/screens/ProfileFragment.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.example.closedcircuitapplication.R
 import com.example.closedcircuitapplication.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
@@ -16,7 +14,7 @@ class ProfileFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
@@ -25,5 +23,9 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+
        }
+
 }
