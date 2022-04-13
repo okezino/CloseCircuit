f2b9d0c8e49c6c54d680e15ffbc8ca097ea989ad
Abdulqohar <sabdulqohar9@gmail.com>
2022-04-13 12:43:37 +0100

    implemented deeplinking to invitation link fragment

---
+++ app/src/main/java/com/example/closedcircuitapplication/onboarding/utils/Constants.kt
@@ -0,0 +1,3 @@
+package com.example.closedcircuitapplication.onboarding.utils
+
+const val PLACEHOLDER_VIDEO_RESOURCE_LINK = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
